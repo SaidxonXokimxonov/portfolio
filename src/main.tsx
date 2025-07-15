4b2df53507046ec8a7a2a412ef88abe02b0c75b0
SaidxonXokimxonov <saidxonxokimxonov006@gmail.com>
2025-07-15 22:10:00 +0500

    re build

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./app";
 import { BrowserRouter } from "react-router-dom";
+document.body.style.background = '#0F172A'
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
